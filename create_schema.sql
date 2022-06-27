8cc7e15198c9e7c29d8bd67ffb63e39da661d5d4
m-herdegen <93681016+m-herdegen@users.noreply.github.com>
2022-06-27 15:40:12 -0400

    finished

--- create_schema.sql
+++ create_schema.sql
@@ -1,9 +1,30 @@
 -- Schema
 DROP TABLE IF EXISTS students;
 CREATE TABLE students (
-  id           serial PRIMARY KEY,
-  first_name   varchar(255) NOT NULL,
-  last_name    varchar(255) NOT NULL,
-  birthdate    date NOT NULL,
-  address_id   integer
+    id           serial PRIMARY KEY,
+    first_name   varchar(255) NOT NULL,
+    last_name    varchar(255) NOT NULL,
+    birthdate    date NOT NULL,
+    address_id   integer
 );
+DROP TABLE IF EXISTS addresses;
+CREATE TABLE addresses (
+    id          serial PRIMARY KEY,
+    line_1      varchar(255) NOT NULL,
+    city        varchar(255) NOT NULL,
+    state       varchar(255) NOT NULL,
+    zipcode     integer 
+);
+DROP TABLE IF EXISTS classes;
+CREATE TABLE classes (
+    id          serial PRIMARY KEY,
+    name      varchar(255) NOT NULL,
+    credits     integer
+);
+DROP TABLE IF EXISTS enrollments;
+CREATE TABLE enrollments (
+    id          serial PRIMARY KEY,
+    student_id  integer,
+    class_id    integer,
+    grade       varchar(255)
+)
